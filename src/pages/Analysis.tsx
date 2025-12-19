a359ca3012ed69fb013a36f788bd5bab333a767c
thekeenest <kstfbusiness@gmail.com>
2025-12-20 00:29:30 +0300

    fix try for data soruces

--- src/pages/Analysis.tsx
+++ src/pages/Analysis.tsx
@@ -99,7 +99,7 @@ export default function Analysis() {
     queryKey: ['dataSources'],
     queryFn: getDataSources,
   })
-  const dataSources = allDataSources.filter(ds => ds.id !== 'openalex')
+  const dataSources = allDataSources
 
   const { data: queryExamples = {} } = useQuery({
     queryKey: ['queryExamples'],
